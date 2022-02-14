0342ad3fd66516056fd8e422ccc6540126d5b486
Marty <marty@kingkit101.com>
2022-02-14 19:18:39 +0800

    test rmt

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,93 @@
-#include "ws2812.h"
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <soc/rmt_struct.h>
-#include <esp_system.h>
-#include <nvs_flash.h>
-#include <driver/gpio.h>
+#include "main.h"
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "driver/gpio.h"
+#include "driver/rmt.h"
+#include "esp_log.h"
 
-#define WS2812_PIN	18
-#define delay_ms(ms) vTaskDelay((ms) / portTICK_RATE_MS)
+USBCDC USBSerial;
 
-void setup()
+void rgb(uint8_t r, uint8_t g, uint8_t b)
 {
-  ws2812_init(18);
-  const uint8_t anim_step = 10;
-  const uint8_t anim_max = 250;
-  const uint8_t pixel_count = 25; // Number of your "pixels"
-  const uint8_t delay = 25; // duration between color changes
-  rgbVal color = makeRGBVal(anim_max, 0, 0);
-  uint8_t step = 0;
-  rgbVal color2 = makeRGBVal(anim_max, 0, 0);
-  uint8_t step2 = 0;
-  rgbVal *pixels = (rgbVal *) malloc(sizeof(rgbVal) * pixel_count);
-
-  while (1) {
-    color = color2;
-    step = step2;
-
-    for (uint8_t i = 0; i < pixel_count; i++) {
-      pixels[i] = color;
-
-      if (i == 1) {
-        color2 = color;
-        step2 = step;
-      }
-
-      switch (step) {
-      case 0:
-        color.g += anim_step;
-        if (color.g >= anim_max)
-          step++;
-        break;
-      case 1:
-        color.r -= anim_step;
-        if (color.r == 0)
-          step++;
-        break;
-      case 2:
-        color.b += anim_step;
-        if (color.b >= anim_max)
-          step++;
-        break;
-      case 3:
-        color.g -= anim_step;
-        if (color.g == 0)
-          step++;
-        break;
-      case 4:
-        color.r += anim_step;
-        if (color.r >= anim_max)
-          step++;
-        break;
-      case 5:
-        color.b -= anim_step;
-        if (color.b == 0)
-          step = 0;
-        break;
-      }
+  uint8_t idx = 0;
+  for (int8_t i = 7; i > -1; i--)
+  {
+    uint8_t x = (g >> i) & 1;
+    if (x)
+    {
+      rmt_item32_t bit[] = {{{{14, 1, 12, 0}}}};
+      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
     }
-    ws2812_setColors(pixel_count, pixels);
-    delay_ms(delay);
+    else
+    {
+      rmt_item32_t bit[] = {{{{7, 1, 16, 0}}}};
+      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
+    }
   }
 
+  for (int8_t i = 7; i > -1; i--)
+  {
+    uint8_t x = (r >> i) & 1;
+    if (x)
+    {
+      rmt_item32_t bit[] = {{{{14, 1, 12, 0}}}};
+      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
+    }
+    else
+    {
+      rmt_item32_t bit[] = {{{{7, 1, 16, 0}}}};
+      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
+    }
+  }
+
+  for (int8_t i = 7; i > -1; i--)
+  {
+    uint8_t x = (b >> i) & 1;
+    if (x)
+    {
+      rmt_item32_t bit[] = {{{{14, 1, 12, 0}}}};
+      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
+    }
+    else
+    {
+      rmt_item32_t bit[] = {{{{7, 1, 16, 0}}}};
+      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
+    }
+  }
+
+  rmt_tx_start(RMT_CHANNEL_0, 1);
+  rmt_wait_tx_done(RMT_CHANNEL_0, pdMS_TO_TICKS(5)); //tick = 100/80M=1.25us
+  ets_delay_us(10);
+  rmt_tx_stop(RMT_CHANNEL_0);
 }
 
-void loop(){
+void setup(void)
+{
+  rmt_config_t cfg = RMT_DEFAULT_CONFIG_TX(GPIO_NUM_18, RMT_CHANNEL_0);
+  cfg.clk_div = 2;
 
+  USB.begin();
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("GoGoGo...");
+  rmt_config(&cfg);
+  rmt_driver_install(cfg.channel, 0, 0);
+  for (int i = 0; i < 10; i++)
+  {
+    rgb(5, 0, 0);
+    delay(500);
+    Serial.println("next...");
+    rgb(0, 5, 0);
+    delay(500);
+    Serial.println("next...");
+    rgb(0, 0, 5);
+    delay(500);
+  }
+}
+
+void loop()
+{
 }
--- src/main.h
+++ src/main.h
@@ -2,8 +2,6 @@
 #define __MAIN__
 #include "Arduino.h"
 #include "USB.h"
-#include "ESP32TimerInterrupt.h"
-#define TIMER0_INTERVAL_MS 1000
 
 // USB
 extern USBCDC USBSerial;
