cbddb9426f9491b4c55fe95fd77d71d10159359a
Marty <marty@kingkit101.com>
2022-02-15 02:20:51 +0800

    test rmt_fill_tx_items

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,80 @@
 #include "main.h"
-#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
-#include "driver/gpio.h"
+#include "freertos/event_groups.h"
+#include "esp32-hal.h"
 #include "driver/rmt.h"
-#include "esp_log.h"
 
 USBCDC USBSerial;
+#define NR_OF_LEDS 25
+#define NR_OF_ALL_BITS 24 * NR_OF_LEDS
 
-void rgb(uint8_t r, uint8_t g, uint8_t b)
+rmt_data_t led_data[NR_OF_ALL_BITS];
+rmt_obj_t *rmt_send = NULL;
+
+int color[] = {0x5, 0x0, 0x0}; // GRB value
+int led_index = 10;
+
+void flash()
 {
-  uint8_t idx = 0;
-  for (int8_t i = 7; i > -1; i--)
-  {
-    uint8_t x = (g >> i) & 1;
-    if (x)
+    // Init data with only one led ON
+    int led, col, bit;
+    int i = 0;
+    for (led = 0; led < NR_OF_LEDS; led++)
     {
-      rmt_item32_t bit[] = {{{{14, 1, 12, 0}}}};
-      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
+        for (col = 0; col < 3; col++)
+        {
+            for (bit = 0; bit < 8; bit++)
+            {
+                if ((color[col] & (1 << (7 - bit))) && (led == led_index))
+                {
+                    led_data[i].level0 = 1;
+                    led_data[i].duration0 = 8;
+                    led_data[i].level1 = 0;
+                    led_data[i].duration1 = 4;
+                }
+                else
+                {
+                    led_data[i].level0 = 1;
+                    led_data[i].duration0 = 4;
+                    led_data[i].level1 = 0;
+                    led_data[i].duration1 = 8;
+                }
+                i++;
+            }
+        }
     }
-    else
+    // make the led travel in the pannel
+    if ((++led_index) >= NR_OF_LEDS)
     {
-      rmt_item32_t bit[] = {{{{7, 1, 16, 0}}}};
-      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
+        led_index = 0;
     }
-  }
-
-  for (int8_t i = 7; i > -1; i--)
-  {
-    uint8_t x = (r >> i) & 1;
-    if (x)
-    {
-      rmt_item32_t bit[] = {{{{14, 1, 12, 0}}}};
-      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
-    }
-    else
-    {
-      rmt_item32_t bit[] = {{{{7, 1, 16, 0}}}};
-      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
-    }
-  }
-
-  for (int8_t i = 7; i > -1; i--)
-  {
-    uint8_t x = (b >> i) & 1;
-    if (x)
-    {
-      rmt_item32_t bit[] = {{{{14, 1, 12, 0}}}};
-      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
-    }
-    else
-    {
-      rmt_item32_t bit[] = {{{{7, 1, 16, 0}}}};
-      ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_CHANNEL_0, bit, 1, idx++));
-    }
-  }
-
-  rmt_tx_start(RMT_CHANNEL_0, 1);
-  rmt_wait_tx_done(RMT_CHANNEL_0, pdMS_TO_TICKS(5)); //tick = 100/80M=1.25us
-  ets_delay_us(10);
-  rmt_tx_stop(RMT_CHANNEL_0);
+    // Send the data
+    //rmtWrite(rmt_send, led_data, NR_OF_ALL_BITS);
+    Serial.println("fill led");
+    rmt_set_tx_intr_en(RMT_CHANNEL_0, false);
+    rmt_tx_memory_reset(RMT_CHANNEL_0);
+    rmt_tx_start(RMT_CHANNEL_0,true);
+    delay(10);
+    rmt_fill_tx_items(RMT_CHANNEL_0, (const rmt_item32_t *)led_data, NR_OF_LEDS, 0);
+    delay(10);
+    rmt_tx_stop(RMT_CHANNEL_0);
+    Serial.println("done.");
 }
 
-void setup(void)
+void setup()
 {
-  rmt_config_t cfg = RMT_DEFAULT_CONFIG_TX(GPIO_NUM_18, RMT_CHANNEL_0);
-  cfg.clk_div = 2;
-
-  USB.begin();
-  Serial.begin(115200);
-  delay(500);
-  Serial.println("GoGoGo...");
-  rmt_config(&cfg);
-  rmt_driver_install(cfg.channel, 0, 0);
-  for (int i = 0; i < 10; i++)
-  {
-    rgb(5, 0, 0);
-    delay(500);
-    Serial.println("next...");
-    rgb(0, 5, 0);
-    delay(500);
-    Serial.println("next...");
-    rgb(0, 0, 5);
-    delay(500);
-  }
+    USB.begin();
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("GoGOGo...");
+    if ((rmt_send = rmtInit(18, RMT_TX_MODE, RMT_MEM_256)) == NULL)
+    {
+        Serial.println("init sender failed\n");
+    }
+    float realTick = rmtSetTick(rmt_send, 100);
+    Serial.printf("real tick set to: %fns\n", realTick);
+    flash();
 }
 
 void loop()
