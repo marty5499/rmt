875245e64fbc916ac27ca8e13250637f4dfde304
Marty <marty@kingkit101.com>
2022-02-16 20:00:07 +0800

    ws2812b-mini-v3

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "esp32-hal.h"
+#include "esp32-hal-rmt.h"
 #include "ESP32TimerInterrupt.h"
+#include "driver/rmt.h"
+
 ///// rmt //////
 ESP32Timer ITimer0(0);
 
@@ -33,14 +36,7 @@ bool sw = true;
 long lastMsg = 0;
 bool debugState = true;
 
-void rmtSetup()
-{
-    rmt_send = rmtInit(18, RMT_TX_MODE, RMT_MEM_256);
-    float realTick = rmtSetTick(rmt_send, 100); // 100ns
-    Serial.printf("real tick set to: %fns\n", realTick);
-}
-
-void flash()
+void IRAM_ATTR flash()
 {
     int led, col, bit;
     int i = 0;
@@ -52,17 +48,19 @@ void flash()
             {
                 if ((color[col] & (1 << (7 - bit))) && (led == led_index))
                 {
+                    // 設定高電位
+                    //
                     led_data[i].level0 = 1;
-                    led_data[i].duration0 = 9;
+                    led_data[i].duration0 = 7;
                     led_data[i].level1 = 0;
-                    led_data[i].duration1 = 9;
+                    led_data[i].duration1 = 7;
                 }
                 else
                 {
                     led_data[i].level0 = 1;
                     led_data[i].duration0 = 3;
                     led_data[i].level1 = 0;
-                    led_data[i].duration1 = 9;
+                    led_data[i].duration1 = 7;
                 }
                 i++;
             }
@@ -72,6 +70,7 @@ void flash()
     {
         led_index = 0;
     }
+    // rmt_fill_tx_items(RMT_CHANNEL_0, (const rmt_item32_t *)led_data, NR_OF_ALL_BITS, 0);
     rmtWrite(rmt_send, led_data, NR_OF_ALL_BITS);
 }
 
@@ -79,8 +78,8 @@ void TimerHandler0()
 {
     if (refresh)
     {
-        flash();
         refresh = false;
+        flash();
     }
 }
 
@@ -123,26 +122,29 @@ void startWiFi()
     Serial.println("wifi connected.");
 }
 
-void debugMode(bool t)
-{
-    debugState = t;
-    if (debugState)
-    {
-        USB.begin();
-        Serial.begin(115200);
-    }
-}
-
 void remote()
 {
     startWiFi();
     startMQTT();
 }
 
+void rmtSetup()
+{
+    rmt_send = rmtInit(18, RMT_TX_MODE, RMT_MEM_256);
+    float realTick = rmtSetTick(rmt_send, 100); // 100ns
+    Serial.printf("real tick set to: %fns\n", realTick);
+    rmt_tx_start(RMT_CHANNEL_0, true);
+    rmt_set_tx_intr_en(RMT_CHANNEL_0, false);
+    rmt_set_tx_thr_intr_en(RMT_CHANNEL_0, false, 0);
+    // rmt_set_mem_pd(RMT_CHANNEL_0, false);
+    //  rmt_tx_stop(RMT_CHANNEL_0);
+}
+
 void setup()
 {
     setCpuFrequencyMhz(240);
-    debugMode(true);
+    USB.begin();
+    Serial.begin(115200);
     delay(2000);
     rmtSetup();
     Serial.println("GoGoGo...");
