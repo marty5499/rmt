2c3152a1ce62fcccbc4aea61ec14bd0b155ad66c
Marty <marty@kingkit101.com>
2022-02-20 18:34:22 +0800

    test OK!

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,12 @@ void onMsg(String msg)
     i = ++i % 25;
     int8_t j = i - 12;
     j = j < 0 ? -1 * j : j;
-    //ws2812_setLED(12, 0, j, 0);
-    ws2812_setAllLED(0, j, 0);
+    ws2812_setLED(0, 0, j, 0);
+    ws2812_setLED(4, 0, j, 0);
+    ws2812_setLED(12, 0, j, 0);
+    ws2812_setLED(20, 0, j, 0);
+    ws2812_setLED(24, 0, j, 0);
+    // ws2812_setAllLED(0, j, 0);
     ws2812_update();
 }
 
@@ -17,5 +21,7 @@ void setting()
     ws2812_setAllLED(0, 10, 0, true);
     remote();
     ws2812_setAllLED(10, 0, 0, true);
+    delay(1000);
+    ws2812_setAllLED(1, 0, 1, true);
     Serial.println("start...");
 }
